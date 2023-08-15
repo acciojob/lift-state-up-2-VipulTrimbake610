69fb4a30ccb1e0eb225f7253bcbc98409957289e
VipulTrimbake610 <vipultrimbake610@gmail.com>
2023-08-15 13:34:50 +0530

    submitting code-Tue Aug 15 2023 13:34:49 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text,setText] = useState("");
+  function handleChange(e){
+    setText(e.target.value);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <div className="parent">
+          <h1>Parent Component</h1>
+          {text}
+            <div className="child">
+              <h2>Child Component</h2>
+              <input type="text" onChange={handleChange} />
+            </div>
+        </div>
     </div>
   )
 }
